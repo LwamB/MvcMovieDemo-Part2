688f246ec143c9856694c78b19ea17f4b2e9f439
LwamB <Lwamberihu0@gmail.com>
2023-06-03 23:57:02 +0200

    Add project files.

---
+++ MvcMovie/DAL/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace MvcMovie.DAL
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Get(
+    Expression<Func<T, bool>> filter = null,
+    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+    params Expression<Func<T, object>>[] includes);
+
+        T GetByID(int id);
+
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(int id);
+        void DeleteAll();
+        void Save();
+    }
+}
